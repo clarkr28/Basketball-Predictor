180fc5fb657589b4fe577bc11d7e81d57231a90f
Jared Clark <clarkr28@msu.edu>
2017-12-13 02:12:50 -0500

    basketballHTMLParser now returns a 2D list where each row contains all data for a game in the team's season.  The parser itself only returns strings, but strings representing floats/integers were casted to their appropriate type when inserted into the data list.

--- basketballHTMLParser.py
+++ basketballHTMLParser.py
@@ -3,17 +3,44 @@
 from html.parser import HTMLParser
 from enum import Enum
 
+
+# return true if the string passed to the function is a float
+def isFloat(x):
+  try:
+    a = float(x)
+  except ValueError:
+    return False
+  else:
+    # search the string for a decimal point to make sure it's not an integer
+    return x.find('.') != -1
+
+
+# return true if the string passed to the function is an integer
+def isInt(x):
+  try:
+    a = int(x)
+  except ValueError:
+    return False
+  else:
+    # search the string for a decimal point to make sure it's not a float
+    return x.find('.') == -1
+
+
 class ParserState(Enum):
   # define all of the states
   NotInTable = 1
   InTable = 2 
-  InRow = 3
+  InTableBody = 3
+  InRow = 4
 
 
 class GamelogHTMLParser(HTMLParser):
 
   # id of the table
   TableId = 'sgl-basic'
+
+  # full size of a row
+  FullRowSize = 39
 
   # True if the parsing is inside the table, False otherwise
   inTable = False
@@ -31,30 +58,64 @@ class GamelogHTMLParser(HTMLParser):
   def getTable(self):
     return self.seasonData
 
+
   def handle_starttag(self, tag, attrs):
 
-    # if not in a table, check to see if current tag is for a table
-    if self.currState == ParserState.NotInTable:
-      if tag == 'table':
-        # make sure the table id is the correct id
-        for att in attrs:
-          if att[0] == 'id' and att[1] == self.TableId:
-            # id is correct, so update current state
-            self.currState = ParserState.InTable
-            print('start tag:', tag)
+    # if not in a table and the current tag is a table, make sure this is 
+    # the correct table by checking the table id.
+    if self.currState == ParserState.NotInTable and tag == 'table':
+      # make sure the table id is the correct id
+      for att in attrs:
+        if att[0] == 'id' and att[1] == self.TableId:
+          # id is correct, so update current state
+          self.currState = ParserState.InTable
+
+    # move current state to in table body if necessary
+    elif self.currState == ParserState.InTable and tag == 'tbody':
+      self.currState = ParserState.InTableBody
+
+    # move current state to in row if necessary
+    elif self.currState == ParserState.InTableBody and tag == 'tr':
+      self.currState = ParserState.InRow
 
 		
   def handle_endtag(self, tag):
 
-    # if already in a table, check to see if it's the end of a table
-    if self.currState == ParserState.InTable:
-      # see if the current tag is a table
-      if tag == 'table':
-        self.currState = ParserState.NotInTable # no longer in the table
-        print("end tag  :", tag)
+    # if currently in a table and the current tag is a table, set the current
+    # state to not being in a table.
+    if self.currState == ParserState.InTable and tag == 'table':
+      self.currState = ParserState.NotInTable # no longer in the table
+
+    # if currently in the table body and the current tag is is tbody, set the
+    # current state to being in a table.
+    elif self.currState == ParserState.InTableBody and tag == 'tbody':
+      self.currState = ParserState.InTable
+
+    # if currently in a row and current tag is a (table row), set the 
+    # current state to being in a table body, then insert the completed row
+    # into the season data list
+    elif self.currState == ParserState.InRow and tag == 'tr':
+      self.currState = ParserState.InTableBody
+
+      # a home game leaves the game location field blank, which does not get
+      # added to the list.  So if the size of the list is 1 smaller than it
+      # should be, insert a 'H' into the row to show it as a home game.
+      if len( self.rowData ) == self.FullRowSize - 1:
+        self.rowData.insert(2, 'H')
+      self.seasonData.append(self.rowData) # append entire row to season data
+      self.rowData = []                    # set to empty for next row
 
 
   def handle_data(self, data):
 
-    if self.currState == ParserState.InTable:
-      print(data)
+    # if currently in a row, save the data to the row list
+    if self.currState == ParserState.InRow:
+      # if the data is an integer, put it in the list as in integer
+      if isInt(data):
+        self.rowData.append( int(data) )
+      # if the data is a float, put it in the list as a float
+      elif isFloat(data):
+        self.rowData.append( float(data) )
+      # if neither an int or float, leave data as a string
+      else:
+        self.rowData.append( data )
--- getHTML.py
+++ getHTML.py
@@ -4,7 +4,8 @@ import requests
 from basketballHTMLParser import *
 url = 'https://www.sports-reference.com/cbb/schools/michigan-state/2018-gamelogs.html'
 
-response = requests.get(url)
+response = requests.get( url )
 parser = GamelogHTMLParser()
-parser.feed(response.text)
+parser.feed( response.text )
 table = parser.getTable()
+print( table )
